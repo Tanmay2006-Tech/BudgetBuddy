82b476df51ac2ebe909a06544a166ecf9d6163fe
Tanmay2006-Tech <tanmaytripathi7525@gmail.com>
2025-12-26 13:39:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
-const expenseData = {
-  Food: 1200,
-  Travel: 600,
-  Shopping: 800,
-  Rent: 400,
-  Others: 200
+let totalSavings = 0;
+let totalSpend = 0;
+
+let expenses = {
+  Food: 0,
+  Travel: 0,
+  Shopping: 0,
+  Rent: 0,
+  Others: 0
 };
 
-// PIE CHART
-new Chart(document.getElementById("expenseChart"), {
+// CHART
+const chart = new Chart(document.getElementById("expenseChart"), {
   type: "pie",
   data: {
-    labels: Object.keys(expenseData),
+    labels: Object.keys(expenses),
     datasets: [{
-      data: Object.values(expenseData),
+      data: Object.values(expenses),
       backgroundColor: [
-        "#1abc9c",
-        "#3498db",
-        "#9b59b6",
-        "#e67e22",
-        "#e74c3c"
+        "#1abc9c", "#3498db", "#9b59b6", "#e67e22", "#e74c3c"
       ]
     }]
   }
 });
+
+// ADD TRANSACTION
+function addTransaction() {
+  const amount = Number(document.getElementById("amount").value);
+  const type = document.getElementById("type").value;
+  const category = document.getElementById("category").value;
+
+  if (amount <= 0) {
+    alert("Enter valid amount");
+    return;
+  }
+
+  if (type === "deposit") {
+    totalSavings += amount;
+  } else {
+    totalSpend += amount;
+    expenses[category] += amount;
+  }
+
+  updateUI();
+}
+
+// UPDATE DISPLAY
+function updateUI() {
+  document.getElementById("savings").innerText = "₹" + totalSavings;
+  document.getElementById("spend").innerText = "₹" + totalSpend;
+  document.getElementById("balance").innerText = "₹" + (totalSavings - totalSpend);
+
+  chart.data.datasets[0].data = Object.values(expenses);
+  chart.update();
+}
